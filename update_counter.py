87bd9b3ab4334ca3b76b10398949ff6b8469708e
sfauvel <sfauvel@gmail.com>
2024-01-22 22:13:40 +0100

    Update html generator

--- update_counter.py
+++ update_counter.py
@@ -129,25 +129,35 @@ html = f"""
 <html>
     <head>
         <style>
-            table, th, td {{
+            body {
+                font-size: 3em;
+            }
+            table, th, td {
+                font-size: 1em;
                 margin: 1em;
                 border: 1px solid black;
                 border-collapse: collapse;
-            }}
-            td {{
+            }
+            td {
                 padding: 5px;
                 text-align: center;
-            }}
-            tr:nth-child(odd) {{
+            }
+            tr:nth-child(odd) {
                 background-color: #a4c2f7;
-            }}
+            }
+            @media (max-width: 600px) {
+                
+                table {
+                    width: 90%;  
+                }
+            }
         </style>
     </head>
     <body>
         Inscription aux stages
         <table>{html_days}
         </table>
-        dernière mises à jour: {now_string}
+        dernière mises à jour<br>{now_string}
     </body>
 </html>
 """
